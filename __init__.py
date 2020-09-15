eee80e7f14a13e272050a66f8b6769714325d43f
Yonggan <andrechuele@gmail.com>
2020-09-15 11:41:50 +0200

    Add Settings

--- __init__.py
+++ __init__.py
@@ -1,31 +1,38 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
-import deezer_utils
+from . import deezer_utils
+import os
+
 
 class Deezer(CommonPlaySkill):
     def __init__(self):
         super(Deezer, self).__init__()
 
+    def initialize(self):
+        self.track_directory = os.path.join(self.settings.get('music_dir'), "track")
 
     def CPS_match_query_phrase(self, phrase):
+        self.log.info("Check Query Phrase")
         if 'deezer' in phrase:
             return phrase, CPSMatchLevel.GENERIC
-        else:
-            return None
 
-        track_id = deezer_utils.search_first_track(phrase)
-        self.log.info("Track ID:" + track_id)
-        self.speak_dialog("deezer.dialog")
-        if track_id is None:
+        track = deezer_utils.search_first_track(track_name=phrase, arl=self.settings.get('arl'))
+        if track is None:
             return None
         else:
+            track_id = track["id"]
+            self.speak_dialog(key="track_found",
+                              data={'title_short': track["title_short"], 'artist': track['artist']['name']})
+            download_path = deezer_utils.download_track(track_id=track_id,
+                                                        track_directory=self.track_directory,
+                                                        arl=self.settings.get('arl'))
             data = {
-                "track": deezer_utils.track_directory.join(str(track_id)),
+                "track": download_path,
                 "track_id": track_id
             }
 
-            return (phrase, CPSMatchLevel.EXACT, data)
+            return phrase, CPSMatchLevel.EXACT, data
         """ This method responds wether the skill can play the input phrase.
 
             The method is invoked by the PlayBackControlSkill.
@@ -39,20 +46,18 @@ class Deezer(CommonPlaySkill):
 
     def CPS_start(self, phrase, data):
         self.log.info("Track: " + data['track'])
-        self.audioservice(data['track'])
+        self.CPS_play(data['track'])
         """ Starts playback.
 
             Called by the playback control skill to start playback if the
             skill is selected (has the best match level)
         """
-        pass
 
-    @intent_handler(IntentBuilder('').require('Deezer').require('User'))
-    def list_devices(self, message):
-        """ List available devices. """
-        self.speak_dialog('user.name', {'user_name': deezer_utils.get_user_info(deezer_utils.arl)})
+    @intent_handler('user.intent')
+    def speak_user_name(self, message):
+        self.log.info("Username Intent")
+        self.speak_dialog(key='user', data={'user_name': deezer_utils.get_user_info(arl=self.settings.get('arl'))})
 
 
 def create_skill():
     return Deezer()
-
--- deezer_utils.py
+++ deezer_utils.py
@@ -3,23 +3,26 @@ import os
 
 from pydeezer.constants import track_formats
 
-track_directory = os.path.join("music", "track")
-arl = "the_arl_for_testing_hardcoded"
 
-
-def download_track(track_id, arl):
+def download_track(track_id, track_directory, arl):
     deezer = Deezer(arl=arl)
     track = deezer.get_track(track_id)
-    track["download"](os.path.join(track_directory, str(track_id)), quality=track_formats.MP3_320)
+    download_path = os.path.join(track_directory, str(track_id))
+    if not os.path.exists(track_directory):
+        os.makedirs(track_directory)
+
+    track["download"](download_path, quality=track_formats.MP3_320, filename=str(track_id))
+    return os.path.join(download_path, str(track_id) + ".mp3")
 
 
 def search_first_track(track_name, arl):
     deezer = Deezer(arl=arl)
     track_search_results = deezer.search_tracks(query=track_name, limit=1)
     try:
-        return track_search_results[0]["id"]
+        return track_search_results[0]
     except IndexError:
         return None
+
 
 def get_user_info(arl):
     deezer = Deezer(arl=arl)
